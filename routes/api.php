d4f8ba6d7d565f9f2c4d084fbcabea465c832aea
moham <mohammadhamid757@gmail.com>
2021-04-06 16:05:31 +0300

    Add queue and jobs

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Models\Room;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,4 +25,5 @@ Route::group(['middleware' => 'mobile.redirect'], function () {
 });
 
 Route::resource('rooms', RoomController::class);
+Route::get('users/send_email', [UserController::class, 'sendEmail']);
 Route::resource('bookings', BookingController::class);
